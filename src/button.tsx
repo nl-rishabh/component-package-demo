6538acf55af9cb0c123c8ab088d9e9e197d262a9
Rishabh Dixit <rishabh@newslaundry.com>
2023-10-21 22:40:35 +0530

    chore: fixed button color

--- src/button.tsx
+++ src/button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps
 const Button = (props: ButtonProps) => {
   return (
     <button
-      className="flex items-center justify-center px-4 py-3 text-base text-white bg-red-600 rounded-md"
+      className="flex items-center justify-center px-4 py-3 text-base text-white bg-blue-600 rounded-md"
       {...props}
     >
       Button
